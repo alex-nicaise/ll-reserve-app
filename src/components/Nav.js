e05bd407f8f02030704cf7a10ee2ee71833ba793
alex-nicaise <atnicais@gmail.com>
2023-05-12 16:07:53 -0400

    Added smooth scrolling to About Link.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 const Nav = () => {
     return(
         <nav id="widescreen-nav">
             <menu>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="#">About</Link></li>
-                <li><Link to="#">Menu</Link></li>
+                <li><HashLink smooth to="/#about-section">About</HashLink></li>
+                <li><a to="#">Menu</a></li>
                 <li><Link to="reserve">Reserve</Link></li>
-                <li><Link to="#" role="button" className="ghost-button-primary">Order</Link></li>
-                <li><Link to="#">Log In</Link></li>
+                <li><a to="#" role="button" className="ghost-button-primary">Order</a></li>
+                <li><a to="#">Log In</a></li>
             </menu>
         </nav>
     )
