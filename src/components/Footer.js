48b3355d0fb60dde14556ae19a87f1411ce3da2a
alex-nicaise <atnicais@gmail.com>
2023-05-14 17:07:59 -0400

    Finished styling home page. Need to do reserve page and add some functionality to home.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../images/little-lemon-logo.svg";
+import LogoWhite from "../images/ll-logo-white.png";
 import facebookIcon from "../images/icons/facebook-icon.png";
 import twitterIcon from "../images/icons/twitter-icon.png";
 import instaIcon from "../images/icons/instagram-icon.png";
@@ -9,40 +10,50 @@ import tikTokIcon from "../images/icons/tiktok-icon.png";
 const Footer = () => {
     return(
         <article id="footer">
-            <section>
-                <img src={Logo} alt="Little Lemon Logo"/>
-                <h3>Little Lemon Restaurant</h3>
-                <p>
+            <span>
+                <section>
+                    <img src={LogoWhite} alt="Little Lemon Logo" id="footer-logo"/>
+                    <h3>Little Lemon Restaurant</h3>
+                    <p>
                     30 East 62nd Street <br />
                     Chicago, Illinois 10018
-                </p>
-            </section>
-            <section>
-                <div id="contact-footer">
-                   <h3>Contact</h3>
-                    <p>Telephone: <a href="tel: +19999999999">999-999-9999</a></p>
-                    <p>Email: <a href="mailto: littlelemon@gmail.com">littlelemon@gmail.com</a></p> 
-                </div>
-                <div id="footer-menu">
-                    <menu>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Menu</a></li>
-                        <li><a href="#">Reserve</a></li>
-                        <li><a href="#">Order Online</a> </li>
-                    </menu>
-                </div>
-            </section>
-            <section>
-                <h3>Social Media</h3>
-                <div id="social-icons">
-                    <img src={facebookIcon} alt="Little Lemon Facebook Icon"/>
-                    <img src={instaIcon} alt="Little Lemon Instagram Icon"/>
-                    <img src={twitterIcon} alt="Little Lemon Twitter Icon"/>
-                    <img src={tikTokIcon} alt="Little Lemon Tik Tok Icon"/>
-                    <img src={openTableIcon} alt="Little Lemon Open Table Icon"/>
-                </div>
-            </section>
+                    </p>
+                </section>
+                    <section>
+                    <div id="contact-footer">
+                        <h3>Contact</h3>
+                        <p>
+                            Telephone: &nbsp;
+                            <a href="tel: +19999999999">999-999-9999</a>
+
+                        </p>
+                        <p>
+                            Email: &nbsp;
+                            <a href="mailto: littlelemon@gmail.com">littlelemon@gmail.com</a>
+                        </p>
+                    </div>
+                    <div id="footer-menu">
+                        <h3>Links</h3>
+                        <menu>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Menu</a></li>
+                            <li><a href="#">Reserve</a></li>
+                            <li><a href="#">Order Online</a></li>
+                        </menu>
+                    </div>
+                </section>
+                <section>
+                    <h3>Social Media</h3>
+                    <div id="social-icons">
+                        <img src={facebookIcon} alt="Little Lemon Facebook Icon"/>
+                        <img src={instaIcon} alt="Little Lemon Instagram Icon"/>
+                        <img src={twitterIcon} alt="Little Lemon Twitter Icon"/>
+                        <img src={tikTokIcon} alt="Little Lemon Tik Tok Icon"/>
+                        <img src={openTableIcon} alt="Little Lemon Open Table Icon"/>
+                    </div>
+                </section>
+            </span>
             <section>
                 <p>Copyright © 2023 Little Lemon Restaurant. All rights Reserved</p>
             </section>
