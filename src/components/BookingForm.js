987bc5f385ac957c7e1ed0bcdef25a7079272d3d
alex-nicaise <atnicais@gmail.com>
2023-05-16 18:06:40 -0400

    Lifted booking state, added reducer.

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,39 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 
-const BookingForm = () =>{
-
-    const [book, setBook] = useState({
-        date: "",
-        time: "",
-        guests: 2,
-        occasion: "None"
-    });
+const BookingForm = (props) =>{
 
     return(
     <form>
         <label htmlFor="res-date">Date</label>
-        <input type="date" id="res-date" onChange={e=>setBook({...book, date: e.target.value})}/>
+        <input type="date" id="res-date" onChange={e=>props.dispatch({type: "change_date", date: e.target.value})}/>
 
         <label htmlFor="res-time">Time</label>
-        <select id="res-time" onChange={e=>setBook({...book, time: e.target.value})}>
-            <option>17:00</option>
-            <option>18:00</option>
-            <option>19:00</option>
-            <option>20:00</option>
-            <option>21:00</option>
-            <option>22:00</option>
+        <select id="res-time" onChange={e=>props.changeBooking({...props.initBooking, time: e.target.value})}>
+            {props.times.map(time=><option key={time}>{time}</option>)}
         </select>
 
         <label htmlFor="guests">Guests</label>
         <input type="number"
-        placeholder={book.guests}
+        placeholder={props.initBooking.guests}
         min="1"
         max="10"
         id="guests"
-        onChange={e=>setBook({...book, guests: e.target.value})} />
+        onChange={e=>props.changeBooking({...props.initBooking, guests: e.target.value})} />
 
         <label htmlFor="occasion">Occasion</label>
-        <select id="occasion">
+        <select id="occasion" onChange={e=>props.changeBooking({...props.initBooking, occasion: e.target.value})}>
             <option>None</option>
             <option>Birthday</option>
             <option>Anniversary</option>
@@ -41,11 +29,6 @@ const BookingForm = () =>{
         </select>
 
         <input type="submit" value="Submit Reservation"/>
-
-        <h3>Current date: {book.date}</h3>
-        <h3>Current time: {book.time}</h3>
-        <h3>Current Guests: {book.guests}</h3>
-        <h3>Current occasion: {book.occasion}</h3>
     </form>
     )
 }
--- src/components/ReserveMain.js
+++ src/components/ReserveMain.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useReducer } from "react";
 import seatingImg from "../images/ll-restaurant-seating.jpg"
 import BookingForm from "./BookingForm";
 
 const ReserveMain = () => {
+
+    const [book, setBook] = useState({
+        date: "",
+        time: "",
+        guests: 2,
+        occasion: "None"
+    });
+
+    const initializeTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+
+    const updateTimes = (availableTimes, action) =>{
+        if (action.type === "change_date") {
+            setBook({...book, date: action.date});
+            return ["17:00", "18:00", "104:00", "20:00", "21:00", "22:00"]
+        }
+    }
+
+    const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes);
+
     return(
         <main id="reserve-main">
             <article id="reserve-hero"
@@ -10,7 +29,13 @@ const ReserveMain = () => {
                  alt="Booths and tables at Little Lemon."
             ></article>
             <h1>Reserve a Table</h1>
-            <BookingForm/>
+
+            <BookingForm times={availableTimes} dispatch={dispatch} initBooking={book} changeBooking={setBook} />
+
+            <h3>Current date: {book.date}</h3>
+            <h3>Current time: {book.time}</h3>
+            <h3>Current Guests: {book.guests}</h3>
+            <h3>Current occasion: {book.occasion}</h3>
         </main>
     )
 }
